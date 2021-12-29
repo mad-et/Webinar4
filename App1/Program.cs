67a2c0c3218e7eca0a62dddffa0d7220b276d2b5
Екатерина <mad-et@outlook.com>
2021-12-29 16:21:10 +0300

    Добавьте файлы проекта.

---
+++ App1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        delegate int MyDelegate(int a, int b);
+        static void Main(string[] args)
+        {
+            //MyDelegate myDelegate = PowerAB;
+            //int r = myDelegate(2, 10);
+            //Console.WriteLine(r);
+
+            //myDelegate = PowerBA;
+            //r = myDelegate(2, 10);
+            //Console.WriteLine(r);
+
+            MyDelegate myDelegate = PowerAB;
+            myDelegate += PowerBA;
+            myDelegate(2, 10);           
+            Console.ReadKey();
+        }
+
+        static int PowerAB(int a, int b)
+        {
+            int r = (int)Math.Pow(a, b);
+            Console.WriteLine( r);
+            return r;
+        }
+        static int PowerBA(int a, int b)
+        {
+            int r = (int)Math.Pow(b, a);
+            Console.WriteLine(r);
+            return r;
+        }
+    }
+}
+
